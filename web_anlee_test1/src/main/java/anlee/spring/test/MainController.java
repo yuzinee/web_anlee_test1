2c6d4def254c0f315d58ecaf683e849e465134db
yujin <wlsdl9248@naver.com>
2023-12-28 13:47:24 +0900

    231228 이유진 초기화면 설정

--- web_anlee_test1/src/main/java/anlee/spring/test/MainController.java
+++ web_anlee_test1/src/main/java/anlee/spring/test/MainController.java
@@ -3,6 +3,7 @@ package anlee.spring.test;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @Description : 메인 컨트롤러
@@ -13,8 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    public String startController() {
+		
+        return "test_main"; // 이는 실제 JSP 파일의 이름에 맞게 수정해야 합니다.
+    }
+	
 	@RequestMapping("/{url}")
-    public String handleTestRequests(@PathVariable("url") String url) {
+    public String MainController(@PathVariable("url") String url) {
        
         return url;
     }
